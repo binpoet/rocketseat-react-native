c860d90462d61d844c5ac78a58ae5c3b8acc485e
mbeyed <stevec3e@gmail.com>
2020-06-22 16:45:52 -0300

    Discord Clone UI

---
+++ Discord/src/components/ChannelList/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import ChannelButton from '../ChannelButton';
+
+import { Container, Category, AddCategoryIcon } from './styles';
+
+const ChannelList: React.FC = () => {
+   return (
+      <Container>
+         <Category>
+            <span>Canais de Texto</span>
+            <AddCategoryIcon />
+         </Category>
+
+         <ChannelButton channelName='chat-livre' />
+         <ChannelButton channelName='trabalho' />
+         <ChannelButton channelName='Destiny' />
+         <ChannelButton channelName='Path of Exile' />
+         <ChannelButton channelName='World of Warcraft' />
+
+      </Container>
+   );
+}
+
+export default ChannelList;
