5e40ca0ca7eff0a08f264601fee3225bb67ffb63
mbeyed <stevec3e@gmail.com>
2020-08-05 19:57:52 -0300

    Next Level Week 2 - Day 3 - Frontend integrated with API

--- Proffy/web/src/components/TeacherItem/index.tsx
+++ Proffy/web/src/components/TeacherItem/index.tsx
@@ -3,39 +3,51 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+  avatar: string;
+  bio: string;
+  cost: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img
-          src="https://avatars1.githubusercontent.com/u/49247275?s=460&u=a9e83c76788c406802b53783210a84a8ad36ebf1&v=4"
-          alt="Lucas Dib"
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Lucas Dib</strong>
-          <span>Programação</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-        voluptates aliquid numquam tempore eligendi dicta praesentium quis
-        saepe,
-        <br />
-        <br />
-        nobis minima, animi tenetur corrupti! Exercitationem maxime atque
-        voluptate eveniet laborum esse.
-      </p>
-
+      <p>{teacher.bio}</p>
       <footer>
         <p>
-          Preço/Hora <strong>R$ 10,00</strong>
+          Preço/Hora <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a
+          onClick={createNewConnection}
+          target="blank"
+          href={`https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt="Whatsapp" />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
--- Proffy/web/src/pages/TeacherList/index.tsx
+++ Proffy/web/src/pages/TeacherList/index.tsx
@@ -1,35 +1,93 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import Select from '../../components/Select';
+import Input from '../../components/Input';
+import api from '../../services/api';
 
 import './styles.css';
 
 const TeacherList = () => {
+  const [teachers, setTeachers] = useState([]);
+
+  const [subject, setSubject] = useState('');
+  const [week_day, setWeek_day] = useState('');
+  const [time, setTime] = useState('');
+
+  async function searchTeachers(e: FormEvent) {
+    e.preventDefault();
+
+    const response = await api.get('/classes', {
+      params: {
+        subject,
+        week_day,
+        time,
+      },
+    });
+
+    setTeachers(response.data);
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
-        <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject" />
-          </div>
+        <form id="search-teachers" onSubmit={searchTeachers}>
+          <Select
+            name="subject"
+            label="Matéria"
+            value={subject}
+            onChange={(e) => {
+              setSubject(e.target.value);
+            }}
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Física', label: 'Física' },
+              { value: 'História', label: 'História' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Química', label: 'Química' },
+            ]}
+          />
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day" />
-          </div>
+          <Select
+            name="week_day"
+            label="Dia da Semana"
+            value={week_day}
+            onChange={(e) => {
+              setWeek_day(e.target.value);
+            }}
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda' },
+              { value: '2', label: 'Terça' },
+              { value: '3', label: 'Quarta' },
+              { value: '4', label: 'Quinta' },
+              { value: '5', label: 'Sexta' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time" />
-          </div>
+          <Input
+            type="time"
+            name="time"
+            label="Hora"
+            value={time}
+            onChange={(e) => {
+              setTime(e.target.value);
+            }}
+          />
+
+          <button type="submit">Buscar</button>
         </form>
       </PageHeader>
 
       <main>
-        <TeacherItem />
-        <TeacherItem />
+        {teachers.map((teacher: Teacher) => {
+          return <TeacherItem key={teacher.id} teacher={teacher} />;
+        })}
       </main>
     </div>
   );
