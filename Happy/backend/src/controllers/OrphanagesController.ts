0d41d824ead3cec0e0ea4dda36fcb3923d6baa78
mbeyed <stevec3e@gmail.com>
2020-10-13 17:34:23 -0300

    Next Level Week 3 - Day 2

--- Happy/backend/src/controllers/OrphanagesController.ts
+++ Happy/backend/src/controllers/OrphanagesController.ts
@@ -61,7 +61,7 @@ export default {
       latitude: Yup.number().required(),
       longitude: Yup.number().required(),
       about: Yup.string().required().max(300),
-      instruction: Yup.string().required(),
+      instructions: Yup.string().required(),
       opening_hours: Yup.string().required(),
       open_on_weekends: Yup.boolean().required(),
       images: Yup.array(
