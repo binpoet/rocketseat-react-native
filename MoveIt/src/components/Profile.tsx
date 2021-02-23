e81df4c34949ca4071e52d56378d5b9c1da9994c
mbeyed <stevec3e@gmail.com>
2021-02-23 11:29:42 -0300

    NLW 4 - Day 2

---
+++ MoveIt/src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '@styles/components/Profile.module.css';
+
+export default function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src='https://github.com/lucasdibz.png' alt='Lucas Dibz' />
+      <div>
+        <strong>Lucas Dib</strong>
+        <p>
+          <img src='icons/level.svg' alt='Level' />
+          Level 3
+        </p>
+      </div>
+    </div>
+  );
+}
