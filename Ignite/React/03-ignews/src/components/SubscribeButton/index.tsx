f389b57acd21a4297a32d54d5a75cb1d542bb56e
mbeyed <stevec3e@gmail.com>
2021-03-24 09:26:16 -0300

    Ignite - Nextjs

--- Ignite/React/03-ignews/src/components/SubscribeButton/index.tsx
+++ Ignite/React/03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,11 +1,40 @@
+import { signIn, useSession } from 'next-auth/client';
+
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await fetch('/api/subscribe', { method: 'POST' });
+      const json = await response.json();
+
+      const { sessionId } = json;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
-    <button type='button' className={styles.subscribeButton}>
+    <button
+      type='button'
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now!
     </button>
   );
